7889e4496b116cee4936fdeb985233e1e250291f
Luisangelebp <luisangelebp55@gmail.com>
2026-01-23 00:25:13 -0400

    a

--- Backend/build/database.js
+++ Backend/build/database.js
@@ -1,15 +1,17 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+var __importDefault =
+    (this && this.__importDefault) ||
+    function (mod) {
+        return mod && mod.__esModule ? mod : { default: mod };
+    };
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.db = void 0;
-const sqlite3_1 = __importDefault(require("sqlite3"));
-const sqlite_1 = require("sqlite");
+const sqlite3_1 = __importDefault(require('sqlite3'));
+const sqlite_1 = require('sqlite');
 async function initializeDatabase() {
     exports.db = await (0, sqlite_1.open)({
-        filename: "./src/database/taller.db", // aquí se guarda tu BD
-        driver: sqlite3_1.default.Database
+        filename: './database/taller.db', // aquí se guarda tu BD
+        driver: sqlite3_1.default.Database,
     });
     // Inicialización de tabla ejemplo
     await exports.db.exec(`-- Tabla de Clientes
